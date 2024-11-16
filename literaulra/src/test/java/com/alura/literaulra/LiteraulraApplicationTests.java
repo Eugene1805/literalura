482bf90ff5a0e548a4a00244e72bb5dbe0f9a4eb
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2024-11-16 11:55:01 -0600

    Inicio de proyecto

---
+++ literaulra/src/test/java/com/alura/literaulra/LiteraulraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literaulra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraulraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
